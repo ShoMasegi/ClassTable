67f4078b4cc9ea2e0783b4e6a7785f830554a20e
ShoMasegi <m2.b0c27@gmail.com>
2017-11-30 04:18:00 +0900

    #1 deleteの条件を/作成日/と/授業名/に変更

--- app/src/main/java/masegi/sho/mytimetable/data/source/RestoreLocalDataSource.java
+++ app/src/main/java/masegi/sho/mytimetable/data/source/RestoreLocalDataSource.java
@@ -47,19 +47,22 @@ public class RestoreLocalDataSource implements RestoreDataSource, Observer.Setti
     private final String TASK_NAME_IS_NULL = COLUMN_TASK_NAME + " is null";
 
     private RestoreLocalDataSource(@NonNull Context context){
+
         if(context != null){
+
             this.context = context;
             dbHelper = ClassDataHelper.getInstance(context);
             sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-            tableId = sharedPreferences.getInt(
-                    context.getString(R.string.settings_pref_table_id_key), 0);
+            tableId = sharedPreferences.getInt(context.getString(R.string.settings_pref_table_id_key), 0);
         }
 
         MyApp.addSettingsObserver(this);
     }
 
     public static RestoreLocalDataSource getInstance(@NonNull Context context){
+
         if(INSTACE == null){
+
             INSTACE = new RestoreLocalDataSource(context);
         }
         return INSTACE;
@@ -78,29 +81,38 @@ public class RestoreLocalDataSource implements RestoreDataSource, Observer.Setti
         String orderBy = COLUMN_DUEDATE + " ASC";
         Cursor c;
         db.beginTransaction();
-        try{
+        try {
+
             c = db.query(RESTORE_TABLE_NAME,TASK_COLUMNS,
                     selection ,selectionArgs,null,null,orderBy);
             db.setTransactionSuccessful();
-        }finally {
+        }
+        finally {
+
             db.endTransaction();
         }
 
         if(c != null) {
-            tasksList = new ArrayList<Task>();
-            while (c.moveToNext())
-                tasksList.add(cursorToTask(c));
 
+            tasksList = new ArrayList<Task>();
+            while (c.moveToNext()) tasksList.add(cursorToTask(c));
             c.close();
         }
 
         db.close();
-        if(tasksList == null) callback.onDataNotAvailable();
-        else callback.onTaskLoaded(tasksList);
+        if(tasksList == null) {
+
+            callback.onDataNotAvailable();
+        }
+        else {
+
+            callback.onTaskLoaded(tasksList);
+        }
     }
 
     @Override
     public void getAllTask(@NonNull GetTaskCallback callback) {
+
         ArrayList<Task> tasksList = null;
         SQLiteDatabase db = dbHelper.getReadableDatabase();
         String selection = COLUMN_CLASS_TABLE_ID + WHERECLAUSE
@@ -109,80 +121,101 @@ public class RestoreLocalDataSource implements RestoreDataSource, Observer.Setti
         String orderBy = COLUMN_DUEDATE + " ASC";
         Cursor c;
         db.beginTransaction();
-        try{
+        try {
+
             c = db.query(RESTORE_TABLE_NAME,TASK_COLUMNS,
                     selection,selectionArgs,null,null,orderBy);
             db.setTransactionSuccessful();
-        }finally {
+        }
+        finally {
+
             db.endTransaction();
         }
-
         if(c != null) {
-            tasksList = new ArrayList<Task>();
-            while (c.moveToNext())
-                tasksList.add(cursorToTask(c));
-        }
-        if(c != null) c.close();
 
+            tasksList = new ArrayList<Task>();
+            while (c.moveToNext()) tasksList.add(cursorToTask(c));
+        }
+        if(c != null) {
+
+            c.close();
+        }
         db.close();
-        if(tasksList == null) callback.onDataNotAvailable();
-        else callback.onTaskLoaded(tasksList);
+        if(tasksList == null) {
+
+            callback.onDataNotAvailable();
+        }
+        else {
+
+            callback.onTaskLoaded(tasksList);
+        }
     }
 
 
     @Override
     public void getTasks(@NonNull String className,boolean isCompleted, @NonNull GetTaskCallback callback) {
+
         String selection = COLUMN_CLASS_TABLE_ID + WHERECLAUSE
                 + ANDCLAUSE + COLUMN_CLASSNAME_KEY + WHERECLAUSE
                 + ANDCLAUSE + COLUMN_IS_COMPLETED + WHERECLAUSE
                 + ANDCLAUSE + MEMO_IS_NULL;
         int isCompletedTask = isCompleted ? TASK_COMPLETED : TASK_NOT_COMPLETED;
         String[] selectionArgs = { String.valueOf(tableId), className, String.valueOf(isCompletedTask) };
-
         ArrayList<Task> tasksList = getTasks(selection,selectionArgs);
-        if(tasksList == null) callback.onDataNotAvailable();
-        else callback.onTaskLoaded(tasksList);
+        if(tasksList == null) {
+
+            callback.onDataNotAvailable();
+        }
+        else {
+
+            callback.onTaskLoaded(tasksList);
+        }
     }
 
     @Override
     public Task getTask(@NonNull String className, String createDate) {
+
         String selection = COLUMN_CLASS_TABLE_ID + WHERECLAUSE
                 + ANDCLAUSE + COLUMN_CLASSNAME_KEY + WHERECLAUSE
                 + ANDCLAUSE + COLUMN_CREATEDATE + WHERECLAUSE
                 + ANDCLAUSE + MEMO_IS_NULL;
         String[] selectionArgs = { String.valueOf(tableId), className, createDate };
-
         ArrayList<Task> tasks = getTasks(selection,selectionArgs);
         Task task = tasks.get(0);
-
         return task;
     }
 
-    private ArrayList getTasks(@NonNull String selection,@NonNull String[] selectionArgs){
+    private ArrayList getTasks(@NonNull String selection, @NonNull String[] selectionArgs){
+
         ArrayList<Task> tasksList = new ArrayList<Task>();
         Cursor c;
         SQLiteDatabase db = dbHelper.getReadableDatabase();
         db.beginTransaction();
         try {
+
             c = db.query(RESTORE_TABLE_NAME, TASK_COLUMNS,
                     selection, selectionArgs, null, null, null);
             db.setTransactionSuccessful();
-        }finally {
+        }
+        finally {
+
             db.endTransaction();
         }
-
         if(c != null && c.getCount() > 0) {
-            while (c.moveToNext())
-                tasksList.add(cursorToTask(c));
-        }
-        if (c != null) c.close();
-        db.close();
 
+            while (c.moveToNext()) tasksList.add(cursorToTask(c));
+        }
+        if (c != null) {
+
+            c.close();
+        }
+        db.close();
         return tasksList;
     }
 
     @Override
     public String getMemo(@NonNull String className) {
+
         String memo = null;
         SQLiteDatabase db = dbHelper.getReadableDatabase();
         String[] columns = { COLUMN_MEMO };
@@ -192,50 +225,58 @@ public class RestoreLocalDataSource implements RestoreDataSource, Observer.Setti
         String[] selectionArgs = { String.valueOf(tableId), className };
         Cursor c;
         db.beginTransaction();
-        try{
+        try {
+
             c = db.query(RESTORE_TABLE_NAME, columns,
                     selection, selectionArgs,null,null,null);
             db.setTransactionSuccessful();
-        }finally {
+        }
+        finally {
+
             db.endTransaction();
         }
+        if(c != null && c.getCount() > 0) {
 
-        if(c != null && c.getCount() > 0){
             c.moveToFirst();
             memo = c.getString(0);
         }
-        if(c != null) c.close();
+        if(c != null) {
 
+            c.close();
+        }
         db.close();
         return memo;
     }
 
     @Override
     public void saveTask(Task task) {
-        ContentValues values = taskToValues(task);
 
+        ContentValues values = taskToValues(task);
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         db.beginTransaction();
         long error = db.insert(RESTORE_TABLE_NAME,null,values);
         db.setTransactionSuccessful();
         db.endTransaction();
         db.close();
-
         MyApp.notifyRestoreObservers();
     }
 
     @Override
     public void saveMemo(@NonNull String className, String memo) {
 
-        if (getMemo(className) == null){
+        if (getMemo(className) == null) {
+
             insertMemo(className,memo);
-        }else{
+        }
+        else{
+
             updateMemo(className,memo);
         }
         MyApp.notifyRestoreObservers();
     }
 
     private void insertMemo(@NonNull String className,String memo){
+
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put(COLUMN_CLASS_TABLE_ID, tableId);
@@ -249,12 +290,12 @@ public class RestoreLocalDataSource implements RestoreDataSource, Observer.Setti
     }
 
     private void updateMemo(@NonNull String className,String memo){
+
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put(COLUMN_CLASS_TABLE_ID, tableId);
         values.put(COLUMN_CLASSNAME_KEY,className);
         values.put(COLUMN_MEMO,memo);
-
         String where = COLUMN_CLASS_TABLE_ID + WHERECLAUSE
                 + ANDCLAUSE + COLUMN_CLASSNAME_KEY + WHERECLAUSE
                 + ANDCLAUSE + TASK_NAME_IS_NULL;
@@ -269,10 +310,31 @@ public class RestoreLocalDataSource implements RestoreDataSource, Observer.Setti
 
     @Override
     public void updateTask(Task task) {
-        ContentValues values = taskToValues(task);
 
+        ContentValues values = taskToValues(task);
         String selection = COLUMN_CLASS_TABLE_ID + WHERECLAUSE
                 + ANDCLAUSE + COLUMN_CLASSNAME_KEY + WHERECLAUSE
+                + ANDCLAUSE + COLUMN_CREATEDATE + WHERECLAUSE;
+        String[] selectionArgs = {
+                String.valueOf(tableId),
+                task.getClassName(),
+                CalendarToString.calendarToString(task.getCreateDate())
+        };
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        db.beginTransaction();
+        int i = db.update(RESTORE_TABLE_NAME,values,selection,selectionArgs);
+        db.setTransactionSuccessful();
+        db.endTransaction();
+        db.close();
+        MyApp.notifyRestoreObservers();
+    }
+
+    @Override
+    public void deleteTask(Task task) {
+
+        String selection = COLUMN_CLASS_TABLE_ID + WHERECLAUSE
+                + ANDCLAUSE + COLUMN_TASK_NAME + WHERECLAUSE
                 + ANDCLAUSE + COLUMN_CREATEDATE + WHERECLAUSE;
         String[] selectionArgs = {
                 String.valueOf(tableId),
@@ -283,33 +345,10 @@ public class RestoreLocalDataSource implements RestoreDataSource, Observer.Setti
         SQLiteDatabase db = dbHelper.getWritableDatabase();
 
         db.beginTransaction();
-        int i = db.update(RESTORE_TABLE_NAME,values,selection,selectionArgs);
-        db.setTransactionSuccessful();
-        db.endTransaction();
-        db.close();
-        
-        MyApp.notifyRestoreObservers();
-    }
-
-    @Override
-    public void deleteTask(Task task) {
-        String selection = COLUMN_CLASS_TABLE_ID + WHERECLAUSE
-                + ANDCLAUSE + COLUMN_TASK_NAME + WHERECLAUSE
-                + ANDCLAUSE + COLUMN_CREATEDATE + WHERECLAUSE;
-        String[] selectionArgs = {
-                String.valueOf(tableId),
-                task.getTaskName(),
-                CalendarToString.calendarToString(task.getCreateDate())
-        };
-
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-
-        db.beginTransaction();
         db.delete(RESTORE_TABLE_NAME,selection,selectionArgs);
         db.setTransactionSuccessful();
         db.endTransaction();
         db.close();
-        
         MyApp.notifyRestoreObservers();
     }
 
@@ -324,7 +363,8 @@ public class RestoreLocalDataSource implements RestoreDataSource, Observer.Setti
      * ColumnIndex dose not identify.So if you change column order of Table ,
      * absolutely check this columnIndexes.
      */
-    private Task cursorToTask(Cursor cursor){
+    private Task cursorToTask(Cursor cursor) {
+
         Task task = new Task(cursor.getString(0));
         task.setTaskName(cursor.getString(1));
         task.setDueDate(cursor.getString(2));
@@ -336,9 +376,9 @@ public class RestoreLocalDataSource implements RestoreDataSource, Observer.Setti
         return task;
     }
 
-    private ContentValues taskToValues(Task task){
-        ContentValues values = new ContentValues();
+    private ContentValues taskToValues(Task task) {
 
+        ContentValues values = new ContentValues();
         values.put(COLUMN_CLASS_TABLE_ID, tableId);
         values.put(COLUMN_CLASSNAME_KEY,task.getClassName());
         values.put(COLUMN_TASK_NAME,task.getTaskName());
@@ -356,7 +396,6 @@ public class RestoreLocalDataSource implements RestoreDataSource, Observer.Setti
     @Override
     public void notifySettingChanged() {
 
-        tableId = sharedPreferences.getInt(
-                context.getString(R.string.settings_pref_table_id_key), 0);
+        tableId = sharedPreferences.getInt(context.getString(R.string.settings_pref_table_id_key), 0);
     }
 }
