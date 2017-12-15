7ddf751d2bd08fb3312f08ccf0d78f43c2935e3a
ShoMasegi <m2.b0c27@gmail.com>
2017-12-16 02:53:16 +0900

    #14 ClassCardAdapterにDataBindingを導入 + ThemeColorにPrimaryColorDarkを追加

--- app/src/main/java/masegi/sho/mytimetable/domain/value/ThemeColor.java
+++ app/src/main/java/masegi/sho/mytimetable/domain/value/ThemeColor.java
@@ -10,37 +10,36 @@ import masegi.sho.mytimetable.R;
 
 public enum ThemeColor {
 
-    DEFAULT(0, R.style.AppTheme, R.color.default_color),
-    RED(1, R.style.AppTheme_Red, R.color.colorPrimary_red),
-    PURPLE(2, R.style.AppTheme_Purple, R.color.colorPrimary_purple),
-    BLUE(3, R.style.AppTheme_Blue, R.color.colorPrimary_blue),
-    LIGHTBLUE(4, R.style.AppTheme_LightBlue, R.color.colorPrimary_light_blue),
-    TEAL(5, R.style.AppTheme_Teal, R.color.colorPrimary_teal),
-    GREEN(6, R.style.AppTheme_Green, R.color.colorPrimary_green),
-    YELLOW(7, R.style.AppTheme_Yellow, R.color.colorPrimary_yellow),
-    ORANGE(8, R.style.AppTheme_Orange, R.color.colorPrimary_orange),
-    BROWN(9, R.style.AppTheme_Brown, R.color.colorPrimary_brown);
-
-    public enum Type{
-        STRING,COLOR;
-    }
+    DEFAULT(0, R.style.AppTheme, R.color.default_color, R.color.grey600),
+    RED(1, R.style.AppTheme_Red, R.color.colorPrimary_red, R.color.colorPrimaryDark_red),
+    PURPLE(2, R.style.AppTheme_Purple, R.color.colorPrimary_purple, R.color.colorPrimaryDark_purple),
+    BLUE(3, R.style.AppTheme_Blue, R.color.colorPrimary_blue, R.color.colorPrimaryDark_blue),
+    LIGHTBLUE(4, R.style.AppTheme_LightBlue, R.color.colorPrimary_light_blue, R.color.colorPrimaryDark_light_blue),
+    TEAL(5, R.style.AppTheme_Teal, R.color.colorPrimary_teal, R.color.colorPrimaryDark_teal),
+    GREEN(6, R.style.AppTheme_Green, R.color.colorPrimary_green, R.color.colorPrimaryDark_green),
+    YELLOW(7, R.style.AppTheme_Yellow, R.color.colorPrimary_yellow, R.color.colorPrimaryDark_yellow),
+    ORANGE(8, R.style.AppTheme_Orange, R.color.colorPrimary_orange, R.color.colorPrimaryDark_orange),
+    BROWN(9, R.style.AppTheme_Brown, R.color.colorPrimary_brown, R.color.colorPrimaryDark_brown);
 
     int themeId;
     int themeResId;
     int primaryColorResId;
+    int primaryColorDarkResId;
 
     private static ThemeColor[] themeColors = ThemeColor.values();
 
-    private ThemeColor(int themeId, int themeResId, int primaryColorResId){
+    private ThemeColor(int themeId, int themeResId, int primaryColorResId, int primaryColorDarkResId) {
 
         this.themeId = themeId;
         this.themeResId = themeResId;
         this.primaryColorResId = primaryColorResId;
+        this.primaryColorDarkResId = primaryColorDarkResId;
     }
 
-    public int getThemeResId(){ return this.themeResId; }
+    public int getThemeResId() { return this.themeResId; }
     public int getThemeId() { return this.themeId; }
-    public int getPrimaryColorResId(){ return this.primaryColorResId; }
+    public int getPrimaryColorResId() { return this.primaryColorResId; }
+    public int getPrimaryColorDarkResId() { return this.primaryColorDarkResId; }
 
     public static ThemeColor getThemeColor(int themeId) {
 
